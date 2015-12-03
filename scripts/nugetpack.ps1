c97208a7b5de5544088fa7c7dccaaba447fd715b
Daniel Li <danielli90@hotmail.com>
2015-12-02 22:05:02 -0800

    update version

--- scripts/nugetpack.ps1
+++ scripts/nugetpack.ps1
@@ -3,8 +3,14 @@
 $root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
 # $version = [System.Reflection.Assembly]::LoadFile("$root\MarkdownLog\bin\Release\MarkdownLog.dll").GetName().Version
 # $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
-# $versionStr = "1.0.{0}" -f ([Environment]::GetEnvironmentVariable("APPVEYOR_BUILD_NUMBER"))
-$versionStr = "{0}.{1}" -f ([Environment]::GetEnvironmentVariable("APPVEYOR_BUILD_VERSION"), [Environment]::GetEnvironmentVariable("APPVEYOR_BUILD_NUMBER"))
+$versionStr = "1.4.{0}" -f ([Environment]::GetEnvironmentVariable("APPVEYOR_BUILD_NUMBER"))
+# $versionStr = "{0}.{1}" -f ([Environment]::GetEnvironmentVariable("APPVEYOR_BUILD_VERSION"), [Environment]::GetEnvironmentVariable("APPVEYOR_BUILD_NUMBER"))
+#
+# Attempting to build package from 'SparkCLR.compiled.nuspec'.
+# '1.4.1-SNAPSHOT.29.29' is not a valid version string.
+# Parameter name: version
+# NuGet package ok for C:\projects\sparkclr\csharp\SparkCLR.sln
+# ===== Build succeeded for C:\projects\sparkclr\csharp
 
 Write-Host "Setting .nuspec version tag to $versionStr"
 
