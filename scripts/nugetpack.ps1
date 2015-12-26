9225f47f026c94c9f8e837d0ed6f6461e970694b
Daniel Li <danielli90@hotmail.com>
2015-12-26 02:55:29 -0500

    update nuget version using tag

--- scripts/nugetpack.ps1
+++ scripts/nugetpack.ps1
@@ -1,22 +1,24 @@
-# 
-# http://www.codeproject.com/Tips/806257/Automating-NuGet-Package-Creation-using-AppVeyor
 $root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
-# $version = [System.Reflection.Assembly]::LoadFile("$root\MarkdownLog\bin\Release\MarkdownLog.dll").GetName().Version
-# $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
-$versionStr = "1.4.{0}" -f ([Environment]::GetEnvironmentVariable("APPVEYOR_BUILD_NUMBER"))
-# $versionStr = "{0}.{1}" -f ([Environment]::GetEnvironmentVariable("APPVEYOR_BUILD_VERSION"), [Environment]::GetEnvironmentVariable("APPVEYOR_BUILD_NUMBER"))
-#
-# Attempting to build package from 'SparkCLR.compiled.nuspec'.
-# '1.4.1-SNAPSHOT.29.29' is not a valid version string.
-# Parameter name: version
-# NuGet package ok for C:\projects\sparkclr\csharp\SparkCLR.sln
-# ===== Build succeeded for C:\projects\sparkclr\csharp
 
-Write-Host "Setting .nuspec version tag to $versionStr"
+# expected tagname: v{version-string}. E.g., "v1.5.2-snapshot-2", "v1.5.2-prerelease-1"
+$tagName = $env:APPVEYOR_REPO_TAG_NAME
+Write-Host "[nugetpack.ps1] [INFO] tagname=[$tagName]"
 
-$content = (Get-Content $root\csharp\SparkCLR.nuspec)
-$content = $content -replace '\$version\$',$versionStr
+if (($tagName.Length -gt 1) -and ($tagName.SubString(0,1).ToLower() -eq "v"))
+{
+    $len = $tagName.Length - 1
+    $versionStr = $tagName.SubString(1, $len)
 
-$content | Out-File $root\csharp\SparkCLR.compiled.nuspec
+    Write-Host "[nugetpack.ps1] [INFO] Setting .nuspec version tag to $versionStr"
+
+    $content = (Get-Content $root\csharp\SparkCLR.nuspec)
+    $content = $content -replace "<version>.*</version>", "<version>$versionStr</version>"
+    
+    $content | Out-File $root\csharp\SparkCLR.compiled.nuspec
+}
+else
+{
+    Copy-Item $root\csharp\SparkCLR.nuspec $root\csharp\SparkCLR.compiled.nuspec -force
+}
 
 & $root\tools\NuGet.exe pack $root\csharp\SparkCLR.compiled.nuspec
