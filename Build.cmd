11d12f778428abe7f6e3ee697ffc2ab4825c64ac
Daniel Li <danielli90@hotmail.com>
2016-01-08 20:07:34 -0800

    uninstall first

--- Build.cmd
+++ Build.cmd
@@ -58,7 +58,8 @@ echo check for gpg2...
 
 @rem install gnupg for signing
 if not exist "%ProgramFiles(x86)%\GNU\GnuPG\pub\gpg2.exe" (
-    choco install gpg4win-vanilla -y -f
+    choco uninstall gpg4win-vanilla  -y -f
+    choco install gpg4win-vanilla -y -f -d
 )
 
 echo got gpg4win
